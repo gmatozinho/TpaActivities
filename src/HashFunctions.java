d8d550cc5d36a7859a6daa1e647f8852d5806d46
Gustavo Matozinho Lima <gustavomatozinho@gmail.com>
2018-04-05 08:24:03 -0300

    lixo

---
+++ src/HashFunctions.java
@@ -0,0 +1,207 @@
+import java.math.BigInteger;
+
+public class HashFunctions {
+    private static int[] arrayAscii =  new int[100];
+    private static int[] arrayPolynomial =  new int[100];
+    private static int[] arrayBernstein =  new int[100];
+    private static int[] arrayModifiedBernstein =  new int[100];
+    private static int[] arrayFNV =  new int[100];
+    private static int[] arrayJSW =  new int[100];
+    private static int[] arrayELF =  new int[100];
+
+
+    public static void StartArrays(int n){
+
+        int size = Prime.decideArraySize(n);
+        arrayAscii =  new int[size];
+        arrayPolynomial =  new int[size];
+        arrayBernstein =  new int[size];
+        arrayModifiedBernstein =  new int[size];
+        arrayFNV =  new int[size];
+        arrayJSW =  new int[size];
+        arrayELF =  new int[size];
+    }
+
+    public static void ContColision(int sum, int[] array){
+        int index = DefineIndex(sum,array);
+        array[index] += 1;
+    }
+
+    private static int DefineIndex(int sum, int[] array)
+    {
+        return sum % array.length;
+    }
+
+    public static void ContColision(long sum, int[] array){
+        int index = DefineIndex(sum,array);
+        array[index] += 1;
+    }
+
+    private static int DefineIndex(long sum, int[] array)
+    {
+        return (int) sum % array.length;
+    }
+
+    public static void ContColision(BigInteger sum, int[] array){
+        int index = DefineIndex(sum,array);
+        array[index] += 1;
+    }
+
+    private static int DefineIndex(BigInteger sum, int[] array)
+    {
+        return sum.mod(TreatBigInteger.intToBigInteger(array.length)).intValue();
+    }
+
+    public static int Ascii(String word)
+    {
+        int sum = 0;
+        for (int i=0; i< word.length();i++)
+        {
+            sum += word.charAt(i);
+        }
+
+        return sum;
+    }
+
+    public static int Polynomial(String word)
+    {
+        int sum = 0;
+        int N = 42;
+        for (int i=0; i< word.length();i++)
+        {
+            sum += word.charAt(i) * Math.pow(N,i);
+        }
+
+        return sum;
+    }
+
+    public static BigInteger Bernstein(String word)
+    {
+        BigInteger bigIntegerSum = new BigInteger("0");
+        for (int i = 0; i < word.length(); i++)
+        {
+            BigInteger temporary = bigIntegerSum.multiply(new BigInteger("33"));
+            bigIntegerSum = temporary.add(TreatBigInteger.charToBigInteger(word.charAt(i)));
+        }
+
+        return bigIntegerSum;
+
+        /*@ Sem Big Integer
+                long sum = 0;
+
+                for (int i = 0; i < word.length(); i++)
+                {
+                    sum = 33 * sum + word.charAt(i);
+                }
+
+        return sum;*/
+    }
+
+    public static BigInteger ModifiedBernstein(String word)
+    {
+        BigInteger bigIntegerSum = new BigInteger("0");
+
+        for (int i = 0; i < word.length(); i++)
+        {
+            BigInteger temporary = bigIntegerSum.multiply(new BigInteger("33"));
+            bigIntegerSum = temporary.xor(TreatBigInteger.charToBigInteger(word.charAt(i)));
+        }
+
+        return bigIntegerSum;
+
+        /* @ Sem Big Integer
+                int sum=0;
+
+                for (int i = 0; i < word.length(); i++)
+                {
+                    sum = 33 * sum ^ word.charAt(i);
+                }
+
+                return sum;*/
+    }
+
+    public static BigInteger FNV(String word)
+    {
+        BigInteger fnvSum = new BigInteger("2166136261");
+        BigInteger fnvValue = new BigInteger("16777619");
+
+        for (int i = 0; i < word.length(); i++)
+        {
+            fnvSum = (fnvSum.multiply(fnvValue)).or(TreatBigInteger.charToBigInteger(word.charAt(i)));
+        }
+
+        return fnvSum;
+    }
+
+    public static BigInteger JSW(String word)
+    {
+        BigInteger sum = new BigInteger("16777551");
+
+        for (int i = 0; i < word.length(); i++)
+        {
+            sum = (sum.shiftLeft(1).or(sum.shiftRight(31))).or(TreatBigInteger.charToBigInteger(word.charAt(i)));
+        }
+
+        return sum;
+
+        /* @Sem Big Integer
+        long sum = 16777551;
+
+        for (int i = 0; i < word.length(); i++)
+        {
+            sum = (sum << 1 | sum >> 31) ^ word.charAt(i);
+        }
+
+        return sum;*/
+    }
+
+    public static long ELF(String word)
+    {
+        long sum = 0, var;
+
+        for (int i = 0; i < word.length(); i++)
+        {
+            sum = (sum << 4) + word.charAt(i);
+            var = sum & 0xf0000000L;
+
+            if (var != 0)
+            {
+                sum ^= var >> 24;
+            }
+
+            sum &= ~var;
+        }
+
+        return sum;
+    }
+
+
+    public static int[] getArrayAscii() {
+        return arrayAscii;
+    }
+
+    public static int[] getArrayPolynomial() {
+        return arrayPolynomial;
+    }
+
+    public static int[] getArrayBernstein() {
+        return arrayBernstein;
+    }
+
+    public static int[] getArrayModifiedBernstein() {
+        return arrayModifiedBernstein;
+    }
+
+    public static int[] getArrayFNV() {
+        return arrayFNV;
+    }
+
+    public static int[] getArrayJSW() {
+        return arrayJSW;
+    }
+
+    public static int[] getArrayELF() {
+        return arrayELF;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import com.sun.org.apache.bcel.internal.generic.BIPUSH;
-
 import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -13,7 +11,7 @@ public class Main {
 
 
     private static void CallHashsBench() throws IOException {
-        Hash.StartArrays(100);
+        HashFunctions.StartArrays(100);
         HashAscii();
         HashPolynomial();
         HashBernstein();
@@ -28,14 +26,14 @@ public class Main {
         BufferedReader reader = TreatWords.OpenFile("en-usa-20k.txt");
         String text = reader.readLine();
         while (text  != null) {
-            int sum = Hash.Ascii(text);
-            Hash.ContColision(sum,Hash.getArrayAscii());
+            int sum = HashFunctions.Ascii(text);
+            HashFunctions.ContColision(sum, HashFunctions.getArrayAscii());
             text = reader.readLine();
         }
 
         FileWriter file = WorkWithCsvFile.OpenFile("benchAscii.csv");
         int cont = 0;
-        for (int value : Hash.getArrayAscii()) {
+        for (int value : HashFunctions.getArrayAscii()) {
             WorkWithCsvFile.writeLine(file,WorkWithCsvFile.writeResults(cont++,value));
         }
 
@@ -47,15 +45,15 @@ public class Main {
         BufferedReader reader = TreatWords.OpenFile("en-usa-20k.txt");
         String text = reader.readLine();
         while (text  != null) {
-            int sum = Hash.Polynomial(text);
-            Hash.ContColision(sum,Hash.getArrayPolynomial());
+            int sum = HashFunctions.Polynomial(text);
+            HashFunctions.ContColision(sum, HashFunctions.getArrayPolynomial());
             text = reader.readLine();
         }
 
         FileWriter file = WorkWithCsvFile.OpenFile("benchPolynomial.csv");
         int cont = 0;
 
-        for (int value : Hash.getArrayPolynomial()) {
+        for (int value : HashFunctions.getArrayPolynomial()) {
             WorkWithCsvFile.writeLine(file,WorkWithCsvFile.writeResults(cont++,value));
         }
 
@@ -67,15 +65,15 @@ public class Main {
         BufferedReader reader = TreatWords.OpenFile("en-usa-20k.txt");
         String text = reader.readLine();
         while (text  != null) {
-            BigInteger sum = Hash.Bernstein(text);
-            Hash.ContColision(sum,Hash.getArrayBernstein());
+            BigInteger sum = HashFunctions.Bernstein(text);
+            HashFunctions.ContColision(sum, HashFunctions.getArrayBernstein());
             text = reader.readLine();
         }
 
         FileWriter file = WorkWithCsvFile.OpenFile("benchBernstein.csv");
         int cont = 0;
 
-        for (int value : Hash.getArrayBernstein()) {
+        for (int value : HashFunctions.getArrayBernstein()) {
             WorkWithCsvFile.writeLine(file,WorkWithCsvFile.writeResults(cont++,value));
         }
 
@@ -87,15 +85,15 @@ public class Main {
         BufferedReader reader = TreatWords.OpenFile("en-usa-20k.txt");
         String text = reader.readLine();
         while (text  != null) {
-            BigInteger sum = Hash.ModifiedBernstein(text);
-            Hash.ContColision(sum,Hash.getArrayModifiedBernstein());
+            BigInteger sum = HashFunctions.ModifiedBernstein(text);
+            HashFunctions.ContColision(sum, HashFunctions.getArrayModifiedBernstein());
             text = reader.readLine();
         }
 
         FileWriter file = WorkWithCsvFile.OpenFile("benchModifiedBernstein.csv");
         int cont = 0;
 
-        for (int value : Hash.getArrayModifiedBernstein()) {
+        for (int value : HashFunctions.getArrayModifiedBernstein()) {
             WorkWithCsvFile.writeLine(file,WorkWithCsvFile.writeResults(cont++,value));
         }
 
@@ -107,15 +105,15 @@ public class Main {
         BufferedReader reader = TreatWords.OpenFile("en-usa-20k.txt");
         String text = reader.readLine();
         while (text  != null) {
-            BigInteger sum = Hash.FNV(text);
-            Hash.ContColision(sum,Hash.getArrayFNV());
+            BigInteger sum = HashFunctions.FNV(text);
+            HashFunctions.ContColision(sum, HashFunctions.getArrayFNV());
             text = reader.readLine();
         }
 
         FileWriter file = WorkWithCsvFile.OpenFile("benchFNV.csv");
         int cont = 0;
 
-        for (int value : Hash.getArrayFNV()) {
+        for (int value : HashFunctions.getArrayFNV()) {
             WorkWithCsvFile.writeLine(file,WorkWithCsvFile.writeResults(cont++,value));
         }
 
@@ -127,15 +125,15 @@ public class Main {
         BufferedReader reader = TreatWords.OpenFile("en-usa-20k.txt");
         String text = reader.readLine();
         while (text  != null) {
-            BigInteger sum = Hash.JSW(text);
-            Hash.ContColision(sum,Hash.getArrayJSW());
+            BigInteger sum = HashFunctions.JSW(text);
+            HashFunctions.ContColision(sum, HashFunctions.getArrayJSW());
             text = reader.readLine();
         }
 
         FileWriter file = WorkWithCsvFile.OpenFile("benchJSW.csv");
         int cont = 0;
 
-        for (int value : Hash.getArrayJSW()) {
+        for (int value : HashFunctions.getArrayJSW()) {
             WorkWithCsvFile.writeLine(file,WorkWithCsvFile.writeResults(cont++,value));
         }
 
@@ -147,15 +145,15 @@ public class Main {
         BufferedReader reader = TreatWords.OpenFile("en-usa-20k.txt");
         String text = reader.readLine();
         while (text  != null) {
-            long sum = Hash.ELF(text);
-            Hash.ContColision(sum,Hash.getArrayELF());
+            long sum = HashFunctions.ELF(text);
+            HashFunctions.ContColision(sum, HashFunctions.getArrayELF());
             text = reader.readLine();
         }
 
         FileWriter file = WorkWithCsvFile.OpenFile("benchELF.csv");
         int cont = 0;
 
-        for (int value : Hash.getArrayELF()) {
+        for (int value : HashFunctions.getArrayELF()) {
             WorkWithCsvFile.writeLine(file,WorkWithCsvFile.writeResults(cont++,value));
         }
 
