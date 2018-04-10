7ab33e0601b90269daae5e7c060116d68524ec73
Gustavo Matozinho Lima <gustavomatozinho@gmail.com>
2018-04-10 09:15:04 -0300

    ajusts

---
+++ src/main/java/ByteLib/ByteArray.java
@@ -0,0 +1,10 @@
+package ByteLib;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
+public class ByteArray {
+
+}
