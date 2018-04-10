9932e4db73ba2983c56f45b37de80ebfd5251ee1
Gustavo Matozinho Lima <gustavomatozinho@gmail.com>
2018-04-10 08:57:26 -0300

    add maven

---
+++ src/test/java/GHash/MyNodeTest.java
@@ -0,0 +1,25 @@
+package GHash;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyNodeTest {
+
+    @Test
+    public void setKey() {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void setObject() {
+    }
+
+    @Test
+    public void getKey() {
+    }
+
+    @Test
+    public void getObject() {
+    }
+}
