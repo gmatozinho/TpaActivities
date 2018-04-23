c9496902980c91b702f56965f3915c9922809113
Gustavo Matozinho Lima <gustavomatozinho@gmail.com>
2018-04-23 15:32:37 -0300

    finish open address hash

--- src/main/java/DocumentLib/Document.java
+++ src/main/java/DocumentLib/Document.java
@@ -1,11 +1,39 @@
 package DocumentLib;
 
 import HashLib.Core.MyHash;
+import HashLib.Core.MyHashListChain;
+import TreatFilesAndTextLib.TreatWords;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Document {
-    MyHash frequencyTab = null;
+    MyHash<String,Integer> frequencyTab = null;
+    static LinkedList<String> wordsList;
+    static String separator = " ,;:.!?()[]''-/|{}";
 
-    public Document create(String fileName){return null;}
+    public Document()
+    {
+        wordsList = new LinkedList<>();
+        frequencyTab = new MyHashListChain();
+    }
+
+    public static Document create(String fileName) throws IOException {
+        Document document = new Document();
+        BufferedReader reader = TreatWords.OpenFile(fileName);
+        
+        String line = reader.readLine();
+        while (line != null)
+        {
+            splitWord(line);
+            line = reader.readLine();
+        }
+                
+        return document;
+    }
 
     public void makeTabFreq(String fileName){
     }
@@ -13,4 +41,38 @@ public class Document {
     public String save(String fileName){return null;}
 
     public MyHash getTabFreq(){return null;}
+
+
+    static void splitWord(String line)
+    {
+        String word = "";
+        char[] letters = line.toCharArray();
+        char[] separators = separator.toCharArray();
+        LinkedList<Character> list = new LinkedList<>();
+        for (char c : separators) {
+            list.add(c);
+        }
+
+        list.add('\"');
+        list.add(' ');
+
+        System.out.println(separators);
+        for (char letter : letters ) {
+            if(!list.contains(letter)){
+                word=word+letter;
+            }else if(word !="")
+            {
+                wordsList.add(word);
+                word ="";
+            }
+        }
+        if(word !="")
+        {
+            wordsList.add(word);
+        }
+    }
+
+    public LinkedList<String> getWordsList() {
+        return wordsList;
+    }
 }
--- src/main/java/MainTest/Main.java
+++ src/main/java/MainTest/Main.java
@@ -1,8 +1,15 @@
 package MainTest;
 
+import DocumentLib.Document;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Document document = Document.create("document.txt");
+
+        System.out.println(document.getWordsList());
 
     }
 
