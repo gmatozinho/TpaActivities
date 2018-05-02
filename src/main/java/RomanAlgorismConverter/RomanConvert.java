277992e134aca6d00a7fe805d83079d8484472f7
Gmatozinho <gustavomatozinho@gmail.com>
2018-05-02 00:07:34 -0300

    concatenation with string builder

--- src/main/java/RomanAlgorismConverter/RomanConvert.java
+++ src/main/java/RomanAlgorismConverter/RomanConvert.java
@@ -44,63 +44,63 @@ public class RomanConvert {
 
 
     public String toRomano(int number) throws IOException {
-        String m_number="";
+        StringBuilder m_number= new StringBuilder();
 
         if(number < 4000) {
             while (number >= 1000) {
-                m_number += decimalToRoman.findElements(1000);
+                m_number.append(decimalToRoman.findElements(1000));
                 number -= 1000;
             }
             while (number >= 900) {
-                m_number += decimalToRoman.findElements(900);
+                m_number.append(decimalToRoman.findElements(900));
                 number -= 900;
             }
             while (number >= 500) {
-                m_number += decimalToRoman.findElements(500);
+                m_number.append(decimalToRoman.findElements(500));
                 number -= 500;
             }
             while (number >= 400) {
-                m_number += decimalToRoman.findElements(400);
+                m_number.append(decimalToRoman.findElements(400));
                 number -= 400;
             }
             while (number >= 100) {
-                m_number += decimalToRoman.findElements(100);
+                m_number.append(decimalToRoman.findElements(100));
                 number -= 100;
             }
             while (number >= 90) {
-                m_number += decimalToRoman.findElements(90);
+                m_number.append(decimalToRoman.findElements(90));
                 number -= 90;
             }
             while (number >= 50) {
-                m_number += decimalToRoman.findElements(50);
+                m_number.append(decimalToRoman.findElements(50));
                 number -= 50;
             }
             while (number >= 40) {
-                m_number += decimalToRoman.findElements(40);
+                m_number.append(decimalToRoman.findElements(40));
                 number -= 40;
             }
             while (number >= 10) {
-                m_number += decimalToRoman.findElements(10);
+                m_number.append(decimalToRoman.findElements(10));
                 number -= 10;
             }
             while (number >= 9) {
-                m_number += decimalToRoman.findElements(9);
+                m_number.append(decimalToRoman.findElements(9));
                 number -= 9;
             }
             while (number >= 5) {
-                m_number += decimalToRoman.findElements(5);
+                m_number.append(decimalToRoman.findElements(5));
                 number -= 5;
             }
             while (number >= 4) {
-                m_number += decimalToRoman.findElements(4);
+                m_number.append(decimalToRoman.findElements(4));
                 number -= 4;
             }
             while (number >= 1) {
-                m_number += decimalToRoman.findElements(1);
+                m_number.append(decimalToRoman.findElements(1));
                 number -= 1;
             }
 
-            return m_number;
+            return m_number.toString();
         }
 
         return "Nao foi possivel conveter esse valor";
